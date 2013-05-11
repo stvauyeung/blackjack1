c27147d68ca848b7969c442e99b331f80a2e61a5
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2013-05-11 09:40:13 -0700

    calculate_value works

--- blackjack.rb
+++ blackjack.rb
@@ -1,21 +1,22 @@
 def calculate_value(cards)
-  total_value = 0
+  total_value = [0]
   cards.each do |card|
     value_on_card = card[1]
       if (2..10).include? value_on_card.to_i
         card_value = value_on_card.to_i
-      elsif value_on_card == 'J'||'Q'||'K'
+      elsif value_on_card == 'J' or value_on_card == 'Q'or value_on_card == 'K'
         card_value = 10
       elsif value_on_card == 'A'
-        if total_value < 10
-          card_value = 11
-        else
+        if total_value.inject{|sum,x| sum + x } > 10
           card_value = 1
+        else
+          card_value = 11
         end 
       end
-      total_value =+ card_value
+      total_value << card_value
   end
-  total_value
+  sum_values = total_value.inject{|sum,x| sum + x }
+  sum_values
 end
 
 def deal_to(player, deck)
@@ -37,8 +38,8 @@ dealer_cards = []
 2.times do deal_to(dealer_cards, deck) end
 2.times do deal_to(player_cards, deck) end
 
-# player_value = calculate_value(player_cards)
-# dealer_value = calculate_value(dealer_cards)
+player_value = calculate_value(player_cards)
+dealer_value = calculate_value(dealer_cards)
 
-puts "Player holds: #{player_cards}. Total value: #{calculate_value(player_cards)}" 
-puts "Dealer holds: " + dealer_cards.to_s
+puts "Player holds: #{player_cards}. Total value: #{player_value}" 
+puts "Dealer holds: #{dealer_cards}. Total value: #{dealer_value}"
